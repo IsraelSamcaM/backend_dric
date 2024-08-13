0a4899310ca332796974e1fdc1fbdbcf4c6472a0
jozs83 <samca83@gmail.com>
2024-08-13 15:26:28 -0400

    serviciode tabla listo

--- src/controllers/problematicas.controller.js
+++ src/controllers/problematicas.controller.js
@@ -3,6 +3,19 @@ import { Carrera } from '../models/Carrera.js';
 import { Auxiliar } from '../models/Auxiliar.js';
 import { Solicitante } from '../models/Solicitante.js';
 import { Publicacion } from '../models/Publicacion.js';
+import { where } from 'sequelize';
+
+
+// export const formatDate = (date) => {
+//     return date
+//         ? new Date(date).toLocaleDateString('es-ES', {
+//             day: '2-digit',
+//             month: '2-digit',
+//             year: 'numeric'
+//         })
+//         : null;
+// };
+
 
 export const getProblematicas = async (req, res) => {
     try {
@@ -27,7 +40,6 @@ export const getProblematicas = async (req, res) => {
                 }
             ]
         });
-        //console.log(problematicas.publicaciones[0])
 
         const result = problematicas.map(problematica => {
 
@@ -63,6 +75,67 @@ export const getProblematicas = async (req, res) => {
     }
 }
 
+
+
+export const getTableProblematicas = async (req, res) => {
+    try {
+        const problematicas = await Problematica.findAll({
+            include: [
+                {
+                    model: Auxiliar,
+                    include: [
+                        {
+                            model: Carrera
+                        }
+                    ]
+                },
+                {
+                    model: Solicitante
+                },
+                {
+                    model: Publicacion,
+                }
+            ]
+        });
+
+        const result = problematicas.map(problematica => {
+
+            const carreras = problematica.auxiliares.map(aux => aux.carrera);
+
+            const recentPublication = problematica.publicaciones.reduce((latest, publication) => {
+                return publication.createdAt > latest.createdAt ? publication : latest;
+            }, { createdAt: new Date(0), activo: false }); 
+
+            return {
+                id_problematica: problematica.id_problematica,
+                titulo: problematica.titulo,
+                planteamiento: problematica.planteamiento,
+                causas: problematica.causas,
+                efectos: problematica.efectos,
+                que: problematica.que,
+                como: problematica.como,
+                para_que: problematica.para_que,
+                cuando: problematica.cuando,
+                contacto: problematica.contacto,
+                telefono: problematica.telefono,
+                //fecha: problematica.fecha,
+                zona: problematica.zona,
+                publicado: recentPublication.createdAt, 
+                activo: recentPublication.activo, 
+                actualizado: problematica.updatedAt,
+                creado: problematica.createdAt,
+                solicitante_id: problematica.solicitante_id,
+                solicitante: problematica.solicitante,
+                carreras: carreras
+            };
+        });
+        return res.json(result);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+
 export const getProblematica = async (req, res) => {
     const { id_problematica } = req.params;
 
@@ -95,8 +168,7 @@ export const getProblematica = async (req, res) => {
         }
 
         const carreras = problematica.auxiliares.map(aux => aux.carrera);
-        const publication = problematica.publicaciones[0]; // Acceder a la primera publicación activa
-
+        const publication = problematica.publicaciones[0]; 
         const result = {
             id_problematica: problematica.id_problematica,
             titulo: problematica.titulo,
@@ -111,7 +183,7 @@ export const getProblematica = async (req, res) => {
             telefono: problematica.telefono,
             fecha: problematica.fecha,
             zona: problematica.zona,
-            publicado: publication ? publication.createdAt : null, // Solo muestra el `createdAt`
+            publicado: publication ? publication.createdAt : null, 
             actualizado: problematica.updatedAt,
             creado: problematica.createdAt,
             solicitante_id: problematica.solicitante_id,
@@ -144,7 +216,6 @@ export const createProblematica = async (req, res) => {
             fecha,
             zona,
             solicitante_id: id_solicitante,
-            publicado: true
         });
 
         for (const carreraId of id_carrera) {
@@ -153,11 +224,18 @@ export const createProblematica = async (req, res) => {
                 carrera_id: carreraId  
             });
         }
-        const prueba = true 
-        if (prueba){
+
+        const p = false
+
+        if (p){
             await Publicacion.create({
                 problematicaIdProblematica: newProblematica.id_problematica,
                 activo: true
+            })
+        }else{
+            await Publicacion.create({
+                problematicaIdProblematica: newProblematica.id_problematica,
+                activo: false
             })
         }
 
@@ -170,8 +248,8 @@ export const createProblematica = async (req, res) => {
 
 export const updateProblematica = async (req, res) => {
     const { id_problematica } = req.params;
-    const { titulo, planteamiento,causas,efectos,que,como,para_que,cuando,contacto,
-            telefono,fecha,zona, id_solicitante,id_carrera } = req.body;
+    const { titulo, planteamiento,causas,efectos,que,como,para_que,cuando,contacto,telefono,fecha,zona,
+        id_solicitante, id_carrera } = req.body;
 
     try {
         const problematica = await Problematica.findOne({ where: { id_problematica } });
@@ -223,6 +301,12 @@ export const deleteProblematica = async (req, res) => {
             }
         });
 
+        await Publicacion.destroy({
+            where: {
+                problematicaIdProblematica: id_problematica
+            }
+        });
+
         await Problematica.destroy({ where: { id_problematica } });
         
         res.sendStatus(204);
@@ -230,3 +314,37 @@ export const deleteProblematica = async (req, res) => {
         return res.status(500).json({ message: error.message });
     }
 }
+
+export const updatePublishProblematica = async (req, res) => {
+    const { id_problematica } = req.params;
+    const { publicado } = req.body;
+
+    try {
+        const problematica = await Problematica.findByPk(id_problematica);
+
+        if (!problematica) return res.status(404).json({ message: "La Problematica no existe" });
+
+        if (publicado) {
+            await Publicacion.update(
+                { activo: false },
+                { where: { problematicaIdProblematica: id_problematica } }
+            );
+
+            await Publicacion.create({
+                problematicaIdProblematica: id_problematica,
+                activo: true,
+            });
+        } else {
+            await Publicacion.update(
+                { activo: false },
+                { where: { problematicaIdProblematica: id_problematica } }
+            );
+        }
+
+        res.status(200).json({ message: "Publicacion actualizada correctamente" });
+        
+    } catch (error) {
+        console.error("Error al actualizar la publicacion: ", error);
+        res.status(500).json({ message: "Error interno del servidor" });
+    }
+};
