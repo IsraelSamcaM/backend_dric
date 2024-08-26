5beaa560115a458c5d7142ff193f07ae5ec6a337
AleMonCayola <alejandrocayola19@gmail.com>
2024-08-26 17:36:59 -0400

    rateLimit, dashboard

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import rateLimit from "express-rate-limit";
 //import morgan from "morgan";
 
 const app = express();
@@ -10,12 +11,19 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Hemos recibido demasiadas solicitudes de esta IP. Espere 1 hora'
+});
+
 //Import routes
 
 import usuariosRoutes from "./routes/usuarios.routes.js";
 import solicitantesRoutes from "./routes/solicitantes.routes.js";
 import carrerasRoutes from "./routes/carreras.routes.js";
 import problematicasRoutes from "./routes/problematicas.routes.js";
+import dashboardRoutes from "./routes/dashboard.routes.js";
 
 // Middlewares
 
@@ -24,6 +32,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', limiter)
 // Routes
 //app.use(ambientesRoutes);
 
@@ -31,6 +41,7 @@ app.use("/api/usuarios", usuariosRoutes );
 app.use("/api/solicitantes", solicitantesRoutes );
 app.use("/api/carreras", carrerasRoutes );
 app.use("/api/problematicas", problematicasRoutes );
+app.use('/api', dashboardRoutes);
 
 app.get("/", (req, res) => {
     res.json({ message: {
