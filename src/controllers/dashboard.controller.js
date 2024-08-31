6067d4f931c66f9b0baa8c5c4c3efd8c88830118
AleMonCayola <alejandrocayola19@gmail.com>
2024-08-30 21:09:33 -0400

    Mas detalles Dashboard

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,19 +1,60 @@
 import { Problematica } from '../models/Problematica.js';
 import { Solicitante } from '../models/Solicitante.js';
 import { Usuario } from '../models/Usuario.js';
+import { Publicacion } from '../models/Publicacion.js';
 
 export const getContador = async (req, res) => {
     try {
-        const problematicasCount = await Problematica.count();
-        const solicitudesCount = await Solicitante.count();
-        const usuariosCount = await Usuario.count();
+        const [
+            problematicasCount,
+            entidadesCount,
+            usuariosCount,
+            publicadasCount,
+            noPublicadasCount,
+            institucionesCount,
+            municipiosCount,
+            activosCount,
+            inactivosCount,
+            solicitudesCount
+        ] = await Promise.all([
+            Problematica.count(),
+            Solicitante.count(),
+            Usuario.count(),
+            Problematica.count({
+                include: {
+                    model: Publicacion,
+                    where: { activo: true },
+                    required: true
+                }
+            }),
+            Problematica.count({
+                include: {
+                    model: Publicacion,
+                    where: { activo: false },
+                    required: true
+                }
+            }),
+            Solicitante.count({ where: { tipo_solicitante: 'INSTITUCION' } }),
+            Solicitante.count({ where: { tipo_solicitante: 'MUNICIPIO' } }),
+            Usuario.count({ where: { disponible: true } }),
+            Usuario.count({ where: { disponible: false } }),
+            Problematica.count({where:{ validado: false }})
+        ]);
 
-        res.json({
+        res.status(200).json({
             problematicas: problematicasCount,
-            solicitudes: solicitudesCount,
-            usuarios: usuariosCount
+            entidades: entidadesCount,
+            usuarios: usuariosCount,
+            publicadas: publicadasCount,
+            noPublicadas: noPublicadasCount,
+            instituciones: institucionesCount,
+            municipios: municipiosCount,
+            activos: activosCount,
+            inactivos: inactivosCount,
+           solicitudes:solicitudesCount
         });
     } catch (error) {
-        return res.status(500).json({ message: error.message });
+        console.error("Error al obtener los datos del dashboard:", error);
+        res.status(500).json({ error: "Error al obtener los datos del dashboard" });
     }
-}
+};
--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -4,5 +4,6 @@ import { getContador } from '../controllers/dashboard.controller.js';
 const router = Router();
 
 router.get('/contador', getContador);
+//router.get('/contador/detalles', getDetalles);
 
 export default router;
